57a3815513649ab1ec1c5516c2b874bd2f499ee4
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-11-21 14:17:55 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Logo from "./MainLogo.PNG"
 function App(){
   return(
     <BrowserRouter>
     <div className="grid-container">
             <header className="row">
                 <div>
+                {/* <a className="brand" href="/"><img src={Logo} style={{maxWidth: '30px', maxHeight: '30px'}}alt="Logo" /></a> */}
                     <a className="brand" href="/">Sahayata</a>
                 </div>
                 <div>
