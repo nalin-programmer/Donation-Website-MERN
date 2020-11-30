1255ef4271f99608becce75336e485e67477f12a
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-11-30 10:12:51 +0530

    heroku

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -7,7 +7,7 @@ const productRouter = express.Router();
 
 productRouter.get('/' ,expressAsyncHandler(async(req, res) => {
     const products = await Product.find({});
-    console.log(products);
+    console.log("aaa" + products);
     res.send(products);
 }));
 
