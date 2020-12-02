a640a4a779ebd6b94b99ce600efa97dc0022fd8c
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-02 15:47:36 +0530

    regster

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userConstants"
+import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userConstants"
 
 export const signin = (email, password) => async(dispatch) => {
     dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
