d50c85d5df78c4ecb3bafbd03f3bbe2c496345e0
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2020-12-04 18:53:26 +0530

    place request backend

--- backend/utils.js
+++ backend/utils.js
@@ -17,10 +17,9 @@ export const isAuth = (req, res, next) => {
     const authorization = req.headers.authorization;
     if(authorization){
         const token = authorization.slice(7, authorization.length);
-        console.log(authorization.slice(7, authorization.length));
         jwt.verify(token,process.env.JWT_SECRET || 'somethingsecret', (error, decode) => {
             if(error){
-                res.status(401).send({message: "Invalid Token"});
+                res.status(401).send({message: authorization.slice(7, authorization.length)});
                 
             }else{
                 req.user = decode;
