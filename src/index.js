85c9b06312ebfc4db35676b42d0d90f9136d880c
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-03-23 08:45:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,7 @@ import './index.css';
 import store from './store';
 import firebase from "firebase";
 var firebaseConfig = {
-  apiKey: "AIzaSyBhbNOt5zf5OkuZ-k0lDOFwEL2nCVa_n28",
-  authDomain: "sahayata-8e3ae.firebaseapp.com",
-  databaseURL: "https://sahayata-8e3ae.firebaseio.com",
-  projectId: "sahayata-8e3ae",
-  storageBucket: "sahayata-8e3ae.appspot.com",
-  messagingSenderId: "224007737077",
-  appId: "1:224007737077:web:c12fdbb0f1c5f36d88ffbc",
-  measurementId: "G-XRPYZX2KSM"
+  // firebase config data
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
