809f75ef32304229b4508375f7891271a275129d
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-07-28 16:47:02 +0530

    Update utils.js

--- backend/utils.js
+++ backend/utils.js
@@ -19,7 +19,7 @@ export const isAuth = (req, res, next) => {
     const authorization = req.headers.authorization;
     if(authorization){
         const token = authorization.slice(7, authorization.length);
-        jwt.verify(token,process.env.JWT_SECRET || 'somethingsecret', (error, decode) => {
+        jwt.verify(token,process.env.JWT_SECRET , (error, decode) => {
             if(error){
                 res.status(401).send({message: "Invalid Token"});
                 
